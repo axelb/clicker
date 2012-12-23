1960dfbf65a3c6dfd6ecafbce9a09de25918aa46
Axel Boettcher <ab@cs.hm.edu>
2012-12-23 16:25:33 +0100

    Wired up to answering.

--- routes/vote.js
+++ routes/vote.js
@@ -1,31 +1,33 @@
 var mongoose = require('mongoose')
   , Schema = mongoose.Schema
   , Alternative = new Schema({
-      title: { type: String, required: true, trim: true }
-    });
+        title: { type: String, required: true, trim: true }
+      })
+  , voteSchema = new Schema({
+        question      : { type: String, required: true, trim: true },
+        alternatives  : [Alternative]
+      })
+  , connection = mongoose.createConnection("mongodb://dl5mfx:tyre2hush7pal@ds043997.mongolab.com:43997/onlineresponse")
+  , Vote = connection.model('Vote', voteSchema);
 
-var voteSchema = new Schema({
-    question      : { type: String, required: true, trim: true }
-  , alternatives  : [Alternative]
-});
-
-var connection = mongoose.createConnection("mongodb://dl5mfx:tyre2hush7pal@ds043997.mongolab.com:43997/onlineresponse");
 connection.on('error', function(error){console.log("Connection error: " + error)});
 
-exports.show = function(req, res){
-  res.render('vote', { id: req.params.id });
+exports.show = function(req, res) {
+  Vote.find()
+    .where('_id').equals(req.params.id)
+    .exec(function(error, data){
+           res.render('vote', {vote: data[0]});
+    });
 };
 
 exports.save = function(req, res) {
-var Vote = connection.model('Vote', voteSchema)
-  , newVote = new Vote(req.body);
+var newVote = new Vote(req.body);
   console.log(req.body);
   newVote.save(function(){console.log(newVote);});
   res.end();
 }
 
 exports.list = function(req, res) {
-	var Vote = connection.model('Vote', voteSchema);
 	Vote.find().exec(
 		function(error, data) {
 			res.end(JSON.stringify(data));
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-
+/* express-based server.
+*/
 var express = require('express')
   , routes = require('./routes')
   , vote = require('./routes/vote')
@@ -7,28 +8,13 @@ var express = require('express')
   , path = require('path')
   , mongoose = require('mongoose')
   , Schema = mongoose.Schema
-  , app = express();
+  , app = express()
+  , connection = mongoose.createConnection("mongodb://dl5mfx:tyre2hush7pal@ds043997.mongolab.com:43997/onlineresponse");
 
-  // dl5mfx  tyre2hush7pal
-
-/*var Alternative = new Schema({
-    title: { type: String, required: true, trim: true }
-});
-
-var voteSchema = new Schema({
-    question      : { type: String, required: true, trim: true }
-  , alternatives  : [Alternative]
-});
-*/
-var connection = mongoose.createConnection("mongodb://dl5mfx:tyre2hush7pal@ds043997.mongolab.com:43997/onlineresponse");
 connection.on('error', function(error){console.log("Connection error: " + error)});
-/*
-var Vote = connection.model('Vote', voteSchema)
-  , woodVote = new Vote({question: "How much wood?", alternatives: [{title: "One"}, {title: "Two"}]});
-woodVote.save(function(b){console.log(b);});
-*/
+
 app.configure(function(){
-  app.set('port', process.env.PORT || 8888);
+  app.set('port', process.env.PORT || 8888);//process.env.PORT for deployment on heroku
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
   app.use(express.favicon());
@@ -36,14 +22,13 @@ app.configure(function(){
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
-  app.use(express.static(path.join(__dirname, 'public')));
+  app.use(express.static(path.join(__dirname, 'public')));// content of public is served statically
 });
 
 app.configure('development', function(){
   app.use(express.errorHandler());
 });
 
-//app.get('/:id', routes.index);
 app.get('/voteqr/:id', voteqr.show);
 app.get('/vote/:id', vote.show);
 app.put('/vote', vote.save);
