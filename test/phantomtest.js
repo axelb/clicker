fd2165cbbc3f17609a75172e434551db00d23712
Axel Boettcher <ab@cs.hm.edu>
2013-01-20 22:37:31 +0100

    Tried to get phantomjs tests running.

--- test/phantomtest.js
+++ test/phantomtest.js
@@ -4,12 +4,24 @@ page.onConsoleMessage = function(msg) { console.log(msg); };
 
 page.onLoadFinished = function() {
     var offsets = page.evaluate(function() {
-        return [$('.alternative').first().offset(), $('.alternative').last().offset(), $('#sendButton').first().offset()];
+        console.log('url:', location.href);
+        var alternatives = $('.alternative');
+        return [alternatives.first().offset(), alternatives.last().offset(), $('#sendButton').first().offset()];
        });
     console.log(offsets[0].top);
+    console.log(offsets[0].left);
     console.log(offsets[1].top);
+    console.log(offsets[1].left);
     console.log(offsets[2].top);
-    page.sendEvent('click', offsets[0].left + 1, offsets[0].top + 1);
+    console.log(offsets[2].left);
+    page.sendEvent('doubleclick', offsets[0].left - 1, offsets[0].top - 1);
+    page.sendEvent('click', offsets[0].left, offsets[0].top);
+    page.sendEvent('click', offsets[0].left + 10, offsets[0].top + 1);
+    page.sendEvent('click', offsets[0].left + 2, offsets[0].top + 2);
+    page.sendEvent('click', offsets[0].left + 3, offsets[0].top + 3);
+    page.sendEvent('click', offsets[0].left + 4, offsets[0].top + 4);
+    page.sendEvent('click', offsets[0].left + 5, offsets[0].top + 5);
+
     page.sendEvent('click', offsets[2].left + 1, offsets[2].top + 1);
     page.sendEvent('click', offsets[2].left + 1, offsets[2].top + 1);
     page.sendEvent('click', offsets[2].left + 1, offsets[2].top + 1);
@@ -18,8 +30,8 @@ page.onLoadFinished = function() {
     page.sendEvent('click', offsets[2].left + 1, offsets[2].top + 1);
     page.sendEvent('click', offsets[2].left + 1, offsets[2].top + 1);
     page.sendEvent('click', offsets[1].left + 1, offsets[1].top + 1);
-    page.sendEvent('click', offsets[2].left + 1, offsets[2].top + 1);
-    page.sendEvent('click', offsets[2].left + 1, offsets[2].top + 1);
+    page.sendEvent('click', offsets[2].left + 2, offsets[2].top + 2);
+    page.sendEvent('click', offsets[2].left + 3, offsets[2].top + 3);
     page.render('or.png');
     phantom.exit(0);
 };
