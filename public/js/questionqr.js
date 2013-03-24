cb750339d384e1a3feeceb5ea3308615a6700a08
Axel Boettcher <ab@cs.hm.edu>
2013-03-24 19:18:20 +0100

    Avoid a lonely colon in qr-coded url.

--- public/js/questionqr.js
+++ public/js/questionqr.js
@@ -1,8 +1,8 @@
 var init = function() {
     var host = window.location.hostname
-       , port = window.location.port
+       , port = window.location.port ? ":" + window.location.port : "" // avoid a lonely colon and thus an invalid url in production
        , id = $('#qrcode').attr('value')
        , url = 'http://' + host + ':' + port + '/voteqr/' + id;
     //window.location = url;//creates endless loop of course
-    $('#qrcode').qrcode({width: 1024, height: 1024, text: 'http://' + host + ':' + port + '/question/' + id});
+    $('#qrcode').qrcode({width: 1024, height: 1024, text: 'http://' + host + port + '/question/' + id});
 };
