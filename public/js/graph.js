44d7f245f1596b9d8ddf180dfbc44f7bbbc66102
Axel Boettcher <ab@cs.hm.edu>
2013-03-24 20:21:41 +0100

    Fixed some formatting issues and styling stuff.

--- public/js/graph.js
+++ public/js/graph.js
@@ -1,34 +1,43 @@
-var init = function() {
-  var qid = $('#qid').attr('name');
-  $.ajax('/question/json/' + qid).done(function(question) {
-      drawVisualization(qid, JSON.parse(question));
-  });
+var init = function () {
+    var qid = $('#qid').attr('name')
+        , converter = new Showdown.converter()
+        , questionDiv = $('#question');
+    $.ajax('/question/json/' + qid).done(function (questionJson) {
+        var question = JSON.parse(questionJson)
+            , html = converter.makeHtml(question.question);
+        questionDiv.html(html);
+        drawVisualization(qid, question);
+    });
 };
 
 function drawVisualization(id, question) {
-  $.ajax('/results/' + id).done(function(data) {
-    jQuery('#graph').tufteBar({
-      data: data,
-      barWidth: 0.5, 
-
-      axisLabel: function(index) { 
-        return "";
-      }, 
-
-      color: function(index) {
-        return ['#0040D5', '#C82000', '#49BE00', '#8200B9', '#00C687'][index % 5];
-      },
-
-      legend: {
-        data: question.alternatives,
-        label: function(index) {
-          return question.alternatives[index].title;
-        },
-        color: function(index) {
-          return ['#0040D5', '#C82000', '#49BE00', '#8200B9', '#00C687'][index % 5];
+    $.ajax('/results/' + id).done(function (data) {
+        if(data.length === 0) {
+            $('#result').html("<h2>Vote not open!</h2>");
+            return;
         }
-      }
+        jQuery('#graph').tufteBar({
+            data: data,
+            barWidth: 0.5,
 
+            axisLabel: function (index) {
+                return "";
+            },
+
+            color: function (index) {
+                return ['#0040D5', '#C82000', '#49BE00', '#8200B9', '#00C687'][index % 5];
+            },
+
+            legend: {
+                data: question.alternatives,
+                label: function (index) {
+                    return question.alternatives[index].title;
+                },
+                color: function (index) {
+                    return ['#0040D5', '#C82000', '#49BE00', '#8200B9', '#00C687'][index % 5];
+                }
+            }
+
+        });
     });
-  });
 }
