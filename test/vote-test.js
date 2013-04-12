53a838d6ecdc2bd062b80c852c67273c4dd1ab4b
Axel Boettcher <ab@cs.hm.edu>
2013-04-12 17:35:40 +0200

    More testing, Jenkins-config.

--- test/vote-test.js
+++ test/vote-test.js
@@ -5,7 +5,9 @@
 var casper = require('casper').create({timeout: 20000})
     , utils = require('utils')
     , question = '{"question":"Q","alternatives":[{"title":"A1","$$hashKey":"00E"},{"title":"A2","$$hashKey":"00G"}],"imageId":""}'
-    , response;
+    , response
+    , clickAlternative
+    , i;
 
 casper.start();
 
@@ -21,7 +23,7 @@ casper.open('http://localhost:8888/question', {
 });
 
 // Check the id that is returned by the server.
-casper.then(function() {
+casper.then(function () {
     response = JSON.parse(casper.getHTML("pre"));//dirty hack
     this.test.assertTruthy(response.id, "Repsonse must have contained an id");
 });
@@ -29,11 +31,11 @@ casper.then(function() {
 // Now open the questions's qrcode which also will start a poll.
 casper.then(function () {
     casper.thenOpen("http://localhost:8888/voteqr/" + response.id, function () {
-            casper.waitForSelector('#qrcode');
+        casper.waitForSelector('#qrcode');
     })
 });
 
-casper.then(function() {
+casper.then(function () {
     this.capture('qrcode.png', {
         top: 0,
         left: 0,
@@ -43,9 +45,14 @@ casper.then(function() {
 });
 
 // Now we switch to the student's side and open the question
-casper.then(function() {
-    casper.thenOpen("http://localhost:8888/question/" + response.id , function() {
-            casper.waitForSelector('#alternative0', function() {
+/**
+ * This function can be used to click on different IDs several times
+ * @param alternativeId ID of alternative to click on
+ */
+clickAlternative = function (alternativeId) {
+    casper.then(function () {
+        casper.thenOpen("http://localhost:8888/question/" + response.id, function () {
+            casper.waitForSelector('#alternative' + alternativeId, function () {
                 this.capture('question.png', {
                     top: 0,
                     left: 0,
@@ -53,30 +60,36 @@ casper.then(function() {
                     height: 768
                 });
             });
-    });
-});
-
-// Click on 1st alternative and submit
-casper.then(function() {
-    this.click('#alternative0');
-    this.test.assertExists('#sendButton', "Expect the sendButton here");
-    this.click('#sendButton');
-});
-
-// "danke" schould be shown in return!
-casper.then(function() {
-    casper.waitForSelector('#danke', function(){
-        this.capture('danke.png', {
-            top: 0,
-            left: 0,
-            width: 1024,
-            height: 768
         });
     });
-});
+
+    // Click on 1st alternative and submit
+    casper.then(function () {
+        this.click('#alternative' + alternativeId);
+        this.test.assertExists('#sendButton', "Expect the sendButton here");
+        this.click('#sendButton');
+    });
+
+    // "danke" schould be shown in return!
+    casper.then(function () {
+        casper.waitForSelector('#danke', function () {
+            this.capture('danke.png', {
+                top: 0,
+                left: 0,
+                width: 1024,
+                height: 768
+            });
+        });
+    });
+}
+
+for(i = 0; i < 2; i++) {
+    clickAlternative(0);
+    clickAlternative(1);
+}
 
 // now let's look if there is the correct statistics available
-casper.then(function(){
+casper.then(function () {
     casper.open('http://localhost:8888/results/' + response.id, {
         method: 'get',
         headers: {
@@ -85,14 +98,13 @@ casper.then(function(){
     });
 });
 
-casper.then(function() {
+casper.then(function () {
     var statistics;
     console.log(casper.getHTML());
     statistics = JSON.parse(casper.getHTML("pre"));
     this.test.assertTruthy(statistics[0][1].label, "Repsonse must have one element");
     this.test.assertEquals(statistics[0][1].label, 1, "First label must be 1");
 });
-
 
 
 // RUN THE TESTS
