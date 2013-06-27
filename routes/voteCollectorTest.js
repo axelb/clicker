fd95ef45fed63a4839fb108b580c67999536d3a7
Axel Boettcher <ab@cs.hm.edu>
2013-06-27 17:46:44 +0200

    Adopted the test to new array structure in sc/mc-reponses.

--- routes/voteCollectorTest.js
+++ routes/voteCollectorTest.js
@@ -16,13 +16,13 @@ describe('Vote Collector Test', function () {
     it('should count 2 answers correctly', function () {
         collector.openVote(testId1);
 
-        collector.saveAnswer(testId1, 3);
+        collector.saveAnswer(testId1, [3]);
         assert.equal(collector.getResultsAsJSON(testId1), JSON.stringify([
             [0, {"label": 1}],
             [0, {"label": 2}],
             [1, {"label": 3}]
         ]));
-        collector.saveAnswer(testId1, 3);
+        collector.saveAnswer(testId1, [3]);
         assert.equal(collector.getResultsAsJSON(testId1), JSON.stringify([
             [0, {"label": 1}],
             [0, {"label": 2}],
@@ -32,20 +32,20 @@ describe('Vote Collector Test', function () {
 
     it('should count some more answers correctly', function () {
         collector.openVote(testId2);
-        collector.saveAnswer(testId2, 3);
-        collector.saveAnswer(testId2, 3);
-        collector.saveAnswer(testId2, 3);
-        collector.saveAnswer(testId2, 2);
-        collector.saveAnswer(testId2, 1);
-        collector.saveAnswer(testId2, 1);
+        collector.saveAnswer(testId2, [3]);
+        collector.saveAnswer(testId2, [3]);
+        collector.saveAnswer(testId2, [3]);
+        collector.saveAnswer(testId2, [2]);
+        collector.saveAnswer(testId2, [1]);
+        collector.saveAnswer(testId2, [1, 3]);
         assert.equal(collector.getResultsAsJSON(testId2), JSON.stringify([
             [2, {"label": 1}],
             [1, {"label": 2}],
-            [3, {"label": 3}]
+            [4, {"label": 3}]
         ]));
 
 
-        collector.saveAnswer(testId1, 2);
+        collector.saveAnswer(testId1, [2]);
         assert.equal(collector.getResultsAsJSON(testId1), JSON.stringify([
             [0, {"label": 1}],
             [1, {"label": 2}],
