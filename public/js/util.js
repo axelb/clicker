61d7441eaee94b6bcab403ca3e868a3ddaa5a7e5
Axel Boettcher <ab@cs.hm.edu>
2017-01-03 11:27:10 +0100

    Linting.

--- public/js/util.js
+++ public/js/util.js
@@ -4,13 +4,13 @@
  * Generic function to add events to objects. (see http://ichwill.net)
  */
 function addEvent(obj, evType, fn) {
-  if (obj.addEventListener) { 
-    obj.addEventListener(evType, fn, false); 
-    return true; 
-  } else if (obj.attachEvent) { 
-    var r = obj.attachEvent("on" + evType, fn); 
-    return r;
-  } else { 
-    return false; 
-  }
+    if (obj.addEventListener) { 
+        obj.addEventListener(evType, fn, false); 
+        return true; 
+    } else if (obj.attachEvent) { 
+        var r = obj.attachEvent('on' + evType, fn); 
+        return r;
+    } else { 
+        return false; 
+    }
 }
